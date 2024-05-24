8c5ab05d7a8adc21b833d726f9b5042aaa978ed4
cdkakade <cdkakade1@gmail.com>
2024-05-24 14:53:12 +0530

    added logbook & added trace id in response header

--- src/main/java/expense/manager/gateway/GatewayServerApplication.java
+++ src/main/java/expense/manager/gateway/GatewayServerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
--- src/main/java/expense/manager/gateway/filters/ResponseTraceFilter.java
+++ src/main/java/expense/manager/gateway/filters/ResponseTraceFilter.java
@@ -1,31 +1,35 @@
 package expense.manager.gateway.filters;
 
-import org.springframework.context.annotation.Configuration;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
-//@Configuration
+@Configuration
 public class ResponseTraceFilter {
 
-	private static final Logger logger = LoggerFactory.getLogger(ResponseTraceFilter.class);
+    private static final Logger logger = LoggerFactory.getLogger(ResponseTraceFilter.class);
 
-	@Autowired
-	FilterUtility filterUtility;
+    @Autowired
+    private Tracer tracer;
 
-	@Bean
-	public GlobalFilter postGlobalFilter() {
-		return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
-			HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-			String correlationId = filterUtility.getCorrelationId(requestHeaders);
-			logger.debug("Updated the CORRELATION_ID to the outbound headers. {}",
-					correlationId);
-			exchange.getResponse().getHeaders().add("CORRELATION_ID", correlationId);
-		}));
-	}
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            Span span = tracer.currentSpan();
+            if (span != null) {
+                String traceId = span.context().traceId();
+                logger.info("setting trace id response. {}",
+                        traceId);
+                exchange.getResponse().getHeaders().add("X-Trace-Id", traceId);
+            }
+        }));
+    }
 
 }
