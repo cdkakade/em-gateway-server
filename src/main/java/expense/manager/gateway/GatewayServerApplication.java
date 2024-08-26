d928b49df329dbd384a8800391c171aa6e810567
cdkakade <cdkakade@altimetrik.com>
2024-08-26 10:31:41 +0530

    Forwarded token to downstream microservices

--- src/main/java/expense/manager/gateway/GatewayServerApplication.java
+++ src/main/java/expense/manager/gateway/GatewayServerApplication.java
@@ -1,5 +1,6 @@
 package expense.manager.gateway;
 
+import jakarta.ws.rs.core.HttpHeaders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -10,25 +11,39 @@ import reactor.core.publisher.Hooks;
 @SpringBootApplication
 public class GatewayServerApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Hooks.enableAutomaticContextPropagation();
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes().route(p -> p.path("/currencies/**").uri("lb://CURRENCY-SERVICE"))
-				.route(p -> p.path("/wallets/**")
-						//.filters(f -> f.rewritePath("/currencies/(?<segment>.*)", "/${segment}"))
-						.uri("lb://WALLET-SERVICE"))
-				.route(p -> p.path("/swagger-ui/**")
-						//.filters(f -> f.rewritePath("/currencies/(?<segment>.*)", "/${segment}"))
-						.uri("lb://CURRENCY-SERVICE"))
-				.route(p -> p.path("/openapi/wallets-doc/**").or().query("urls.primaryName", "^wallet")
-						.uri("lb://WALLET-SERVICE"))
-				.route(p -> p.path("/openapi/currencies-doc/**").or().query("urls.primaryName", "^currency")
-						.uri("lb://CURRENCY-SERVICE"))
-				.build();
-	}
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("currency-service", p -> p.path("/currencies/**")
+                        .filters(f -> {
+                            f.tokenRelay();
+                            f.removeRequestHeader(HttpHeaders.COOKIE);
+                            return f;
+                        })
+                        .uri("lb://CURRENCY-SERVICE"))
+                .route("wallet-service", p -> p.path("/wallets/**")
+                        .filters(f -> {
+                            f.tokenRelay();
+                            f.removeRequestHeader(HttpHeaders.COOKIE);
+                            return f;
+                        })
+                        .uri("lb://WALLET-SERVICE"))
+                .route(p -> p.path("/swagger-ui/**")
+                        .uri("lb://CURRENCY-SERVICE"))
+                .route(p -> p.path("/openapi/wallets-doc/**")
+                        .or()
+                        .query("urls.primaryName", "^wallet")
+                        .uri("lb://WALLET-SERVICE"))
+                .route(p -> p.path("/openapi/currencies-doc/**")
+                        .or()
+                        .query("urls.primaryName", "^currency")
+                        .uri("lb://CURRENCY-SERVICE"))
+                .build();
+    }
 
 }
